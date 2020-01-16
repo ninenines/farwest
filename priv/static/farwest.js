830b8e2be17573a4855e626577cd8b6d3cf8a2fc
Loïc Hoguin <essen@ninenines.eu>
2020-01-16 15:26:32 +0100

    JS: Load the final URI on submit
    Also add a very barebone error handling using alert("...")
    to be replaced with something better later on.

--- priv/static/farwest.js
+++ priv/static/farwest.js
@@ -1,16 +1,20 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 	document.querySelectorAll('form[data-operation]').forEach((form) => {
 		form.addEventListener('submit', (e) => {
-			const response = fetch(form.target, {
+			fetch(form.target, {
 				method: form.getAttribute('method'),
 				headers: {
 					'content-type': form.enctype
 				},
 				body: form.elements[0].value,
 				redirect: 'follow'
+			})
+			.then(function(response){
+				if (response.ok)
+					window.location = response.url;
+				else
+					alert("error: " + response.status + " " + response.statusText);
 			});
-
-			// @todo Reload or follow.
 
 			e.preventDefault();
 		});
